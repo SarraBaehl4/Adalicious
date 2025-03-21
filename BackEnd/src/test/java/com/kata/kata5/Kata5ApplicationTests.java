4ef26c7b08460e22936358e9de40279bceec9492
baehl <baehl44@yahoo.fr>
2025-03-21 09:30:46 +0100

    first commit

---
+++ BackEnd/src/test/java/com/kata/kata5/Kata5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.kata5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kata5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
