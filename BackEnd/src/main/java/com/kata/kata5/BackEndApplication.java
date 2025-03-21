2bf7469c74a9ce05d367254a0a3dc216be49f044
baehl <baehl44@yahoo.fr>
2025-03-21 12:44:53 +0100

    correction fichier BackEndApplication

--- BackEnd/src/main/java/com/kata/kata5/BackEndApplication.java
+++ BackEnd/src/main/java/com/kata/kata5/BackEndApplication.java
@@ -1,18 +1,20 @@
 package com.kata.kata5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import me.paulschwarz.springdotenv.DotenvPropertySource;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 @SpringBootApplication
-@RestController
 public class BackEndApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(BackEndApplication.class, args);
-    }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+		// Add DotenvPropertySource to environment before registering components
+    	DotenvPropertySource.addToEnvironment(applicationContext.getEnvironment());
+    	applicationContext.refresh();
+
+		SpringApplication.run(BackEndApplication.class, args);
+	}
 }
